88b18d69ca2139adf3762d35695b3a6f6a8f59c3
Shark <darksidemetatron@gmail.com>
2025-02-27 22:21:45 +0100

    Extend the hacky TransitionalDataRepository with 'clicked url'
    It didn't even have to be this way, the presenter itself could have hold onto it...
    but anyways. the presenter got the GitRepositoriesRepository as well, so favoring works
    and is visible on both screens.
    
    So, with favoring done, and opening the URL done, the only thing left to do is -
    parsing the "contributors_url" response, and drawing them on the screen besides
    making a repository for them.

--- app/src/main/java/com/nsoft/github/data/repository/TransitionalDataRepositoryImpl.kt
+++ app/src/main/java/com/nsoft/github/data/repository/TransitionalDataRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.nsoft.github.data.repository
 
+import android.net.Uri
 import com.nsoft.github.domain.model.GitRepository
 import com.nsoft.github.domain.repository.TransitionalDataRepository
 import javax.inject.Inject
@@ -8,6 +9,7 @@ class TransitionalDataRepositoryImpl @Inject constructor(
     // Since we're not even gonna use sharedpref for this, lets just keep the values in memory
 ): TransitionalDataRepository {
     private lateinit var clickedGitRepo: GitRepository
+    private lateinit var clickedUrl: String
 
     override fun setClickedGitRepo(gitRepository: GitRepository) {
         clickedGitRepo = gitRepository
@@ -15,5 +17,14 @@ class TransitionalDataRepositoryImpl @Inject constructor(
 
     override fun getClickedGitRepo(): GitRepository {
         return clickedGitRepo
+    }
+
+    override fun setClickedUrl(url: String) {
+//        clickedUrl = Uri.parse(url)
+        clickedUrl = url
+    }
+
+    override fun getClickedUrl(): String {
+        return clickedUrl
     }
 }
--- app/src/main/java/com/nsoft/github/presentation/viewmodel/SecondScreenViewModel.kt
+++ app/src/main/java/com/nsoft/github/presentation/viewmodel/SecondScreenViewModel.kt
@@ -4,21 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.nsoft.github.domain.model.FirstScreenErrorState
+import com.nsoft.github.domain.model.GitRepository
 import com.nsoft.github.domain.model.SecondScreenErrorState
 import com.nsoft.github.domain.navigation.FirstScreenNavigationEvent
 import com.nsoft.github.domain.navigation.SecondScreenNavigationEvent
+import com.nsoft.github.domain.repository.GitRepositoriesRepository
 import com.nsoft.github.domain.repository.TransitionalDataRepository
 import com.nsoft.github.domain.usecase.GetRepositoryDetailsUseCase
 import com.nsoft.github.domain.usecase.params.GetRepositoryDetailsUseCaseParams
 import com.nsoft.github.util.MyLogger
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SecondScreenViewModel @Inject constructor(
     private val getRepositoryDetailsUseCase: GetRepositoryDetailsUseCase,
-    private val transitionalDataRepository: TransitionalDataRepository
+    private val transitionalDataRepository: TransitionalDataRepository,
+    private val gitReposRepository: GitRepositoriesRepository
 ): BaseViewModel<SecondScreenNavigationEvent, SecondScreenErrorState>() {
 
     override fun initialNavigationStreamValue() = SecondScreenNavigationEvent.NOWHERE
@@ -41,5 +45,26 @@ class SecondScreenViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    fun getClickedRepo(): GitRepository {
+        return transitionalDataRepository.getClickedGitRepo()
+    }
+
+    fun toggleFavoriteStatus(gitRepo: GitRepository) {
+        gitReposRepository.toggleRepositoryFavoriteStatus(gitRepo)
+    }
+
+    fun isFavoriteRepository(gitRepo: GitRepository): Flow<Boolean> {
+        return gitReposRepository.isRepositoryFavorited(gitRepo)
+    }
+
+    fun onUrlButtonClicked(gitRepo: GitRepository) {
+        transitionalDataRepository.setClickedUrl(gitRepo.htmlUrl)
+        _navigationStream.value = SecondScreenNavigationEvent.PROJECT_URL
+    }
+
+    fun getDestinationUrlString(): String {
+        return transitionalDataRepository.getClickedUrl()
     }
 }
