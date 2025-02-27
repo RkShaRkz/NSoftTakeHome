147343f62bc1b86f121fdaad4469cac05c91f392
Shark <darksidemetatron@gmail.com>
2025-02-27 20:01:03 +0100

    Temporarily hook the thing that doesn't work in hte second screen's presenter

--- app/src/main/java/com/nsoft/github/presentation/viewmodel/SecondScreenViewModel.kt
+++ app/src/main/java/com/nsoft/github/presentation/viewmodel/SecondScreenViewModel.kt
@@ -2,19 +2,45 @@ package com.nsoft.github.presentation.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.nsoft.github.domain.model.FirstScreenErrorState
 import com.nsoft.github.domain.model.SecondScreenErrorState
 import com.nsoft.github.domain.navigation.FirstScreenNavigationEvent
 import com.nsoft.github.domain.navigation.SecondScreenNavigationEvent
+import com.nsoft.github.domain.repository.TransitionalDataRepository
+import com.nsoft.github.domain.usecase.GetRepositoryDetailsUseCase
+import com.nsoft.github.domain.usecase.params.GetRepositoryDetailsUseCaseParams
+import com.nsoft.github.util.MyLogger
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SecondScreenViewModel @Inject constructor(
-
+    private val getRepositoryDetailsUseCase: GetRepositoryDetailsUseCase,
+    private val transitionalDataRepository: TransitionalDataRepository
 ): BaseViewModel<SecondScreenNavigationEvent, SecondScreenErrorState>() {
 
     override fun initialNavigationStreamValue() = SecondScreenNavigationEvent.NOWHERE
     override fun initialErrorStreamValue() = SecondScreenErrorState.NoError
 
+    init {
+        // Observe Room's StateFlow and update the inner MutableStateFlow
+        viewModelScope.launch {
+            getRepositoryDetailsUseCase.executeSuspendWithCallback(
+                GetRepositoryDetailsUseCaseParams(
+                    owner = transitionalDataRepository.getClickedGitRepo().owner.login,
+                    name = transitionalDataRepository.getClickedGitRepo().repoName
+                ),
+            ) { repoDetailsOutcome ->
+                if (repoDetailsOutcome.isSuccessful()) {
+                    val repoDetails = repoDetailsOutcome.getResult()
+                    MyLogger.e("SecondScreenViewModel", "repo details: ${repoDetails}")
+                } else {
+                    val error = repoDetailsOutcome.getError()
+                    MyLogger.e("SecondScreenViewModel", "error: ${error}")
+                }
+            }
+        }
+    }
 }
